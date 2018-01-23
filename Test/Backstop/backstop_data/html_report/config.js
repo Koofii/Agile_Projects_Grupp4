6fed4d4a6793764c23625fd3f525371207c9af2a
Koofii <kristoffer.frejd@gmail.com>
2018-01-23 13:10:53 +0100

    Ok

--- Test/Backstop/backstop_data/html_report/config.js
+++ Test/Backstop/backstop_data/html_report/config.js
@@ -3,10 +3,10 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_My_index_test_0_buttons_0_small.png",
-        "test": "..\\bitmaps_test\\20180116-125025\\backstop_default_My_index_test_0_buttons_0_small.png",
-        "selector": ".buttons",
-        "fileName": "backstop_default_My_index_test_0_buttons_0_small.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_My_index_test_0_ingredienserh1_0_small.png",
+        "test": "..\\bitmaps_test\\20180122-145515\\backstop_default_My_index_test_0_ingredienserh1_0_small.png",
+        "selector": ".ingredienser h1",
+        "fileName": "backstop_default_My_index_test_0_ingredienserh1_0_small.png",
         "label": "My index test",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
@@ -23,10 +23,10 @@ report({
     },
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_My_index_test_0_buttons_1_medium.png",
-        "test": "..\\bitmaps_test\\20180116-125025\\backstop_default_My_index_test_0_buttons_1_medium.png",
-        "selector": ".buttons",
-        "fileName": "backstop_default_My_index_test_0_buttons_1_medium.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_My_index_test_0_ingredienserh1_1_medium.png",
+        "test": "..\\bitmaps_test\\20180122-145515\\backstop_default_My_index_test_0_ingredienserh1_1_medium.png",
+        "selector": ".ingredienser h1",
+        "fileName": "backstop_default_My_index_test_0_ingredienserh1_1_medium.png",
         "label": "My index test",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
@@ -43,10 +43,10 @@ report({
     },
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_My_index_test_0_buttons_2_large.png",
-        "test": "..\\bitmaps_test\\20180116-125025\\backstop_default_My_index_test_0_buttons_2_large.png",
-        "selector": ".buttons",
-        "fileName": "backstop_default_My_index_test_0_buttons_2_large.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_My_index_test_0_ingredienserh1_2_large.png",
+        "test": "..\\bitmaps_test\\20180122-145515\\backstop_default_My_index_test_0_ingredienserh1_2_large.png",
+        "selector": ".ingredienser h1",
+        "fileName": "backstop_default_My_index_test_0_ingredienserh1_2_large.png",
         "label": "My index test",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
